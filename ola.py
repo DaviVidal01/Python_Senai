6dec9cc1699ae49dff22948ea54c852e4f542b79
DaviVidal01 <davividal0605@gmail.com>
2023-05-08 22:22:56 -0300

    Primeira Aula sobre Python, entendendo seus conceitos e fazendo exercícios, incluindo simplificação.

---
+++ ola.py
@@ -0,0 +1,11 @@
+nome = "Davi"
+print("Olá Mundo")
+print("Olá :" + nome)
+
+print("Leia numero: ")
+numero = int(input())
+print("Leia segundo numero: ")
+numero2 = int(input())
+print("Os numeros são: {}".format(numero + numero2))
+
+
---
+++ simp_exercicio.py
@@ -0,0 +1,3 @@
+numero = float(input("Digite um Número: ")) #Desafio de criar o menor código que resolveria o Exercício citado pelo professor
+numero2 = float(input("Digite um segundo Número: "))
+print("Resultados = Multiplicação ({}) | Divisão ({}) | Soma ({}) | Subtração ({})".format(numero*numero2,numero/numero2,numero+numero2,numero-numero2))
